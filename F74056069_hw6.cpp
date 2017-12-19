862fca99283a1fdc73ee804e5501bca1a614ed96
NightKirie <qwer55113322@gmail.com>
2017-12-20 06:23:43 +0800

    update

--- F74056069_hw6.cpp
+++ F74056069_hw6.cpp
@@ -3,36 +3,47 @@
 #include<string>
 #include<iomanip>
 using namespace std;
-typedef struct tree{
-	int height;
-	string parent;
-}Node;
-
-void heightUnion(Node*, int, int);
+void heightUnion(int*, int, int);
+int findroot(int*, int);
 
 int main(){
 	int n;
 	cin >> n;
-	Node node[n];
+	int node[n];
 	for(int i = 0; i < n; i++){
-		node[i].height = 1;
-		node[i].parent = "root";
+		node[i]= -(i+1);
 	}
 	for(int i = 1; i < n; i++)
-		heightUnion(node, 0, i);
+		heightUnion(node, findroot(node, 0), findroot(node, i));
 	cout << setw(8) << std::left << "Node" << "parent" << endl;
-	for(int i = 0; i < n; i++)
-		cout << setw(8) << std::left << i << node[i].parent << endl;
+	for(int i = 0; i < n; i++){
+		cout << setw(8) << std::left << i; 
+		//if(node[i] < 0)
+	//		cout << "root" << endl;
+	//	else
+			cout << node[i] << endl;
+	}
 }
-
-void heightUnion(Node* node, int a, int b){
-	int totalheight = node[a].height + node[b].height;
-	if(node[a].height < node[b].height){
-		node[a].parent = std::to_string(b);
-		node[b].height = totalheight;
+int findroot(int* node, int i){
+	int root;
+	for(root = i; node[root] >= 0; root = node[root])
+		;
+	return root;
+}
+void heightUnion(int* node, int rooti, int rootj){
+	int totalheight;
+	if(node[rooti] == node[rootj])
+		totalheight = node[rooti] - 1;	
+	else if(node[rooti] > node[rootj])
+		totalheight = node[rooti];
+	else
+		totalheight = node[rootj];
+	if(node[rooti] > node[rootj]){
+		node[rooti] = rootj;
+		node[rootj] = totalheight;
 	}
 	else{
-		node[b].parent = std::to_string(a);
-		node[a].height = totalheight;
+		node[rootj] = rooti;
+		node[rooti] = totalheight;
 	}
 }
