d93d76186d4cac71d7e047160c6e1d9d8cbf5f73
NightKirie <qwer55113322@gmail.com>
2018-01-06 09:12:47 +0800

    update

---
+++ F74056069_hw9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int node;
+	cin >> node;
+	int act_num[node], start_ver[node], end_ver[node], act_req[node];
+	for(int i = 0; i < node; i++)
+		cin >> act_num[i] >> start_ver[i] >> end_ver[i] >> act_req[i];
+}
