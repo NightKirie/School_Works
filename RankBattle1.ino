b88c556866e932f7c6ce78d2b58d7ba2e7f26f41
NightKirie <qwer55113322@gmail.com>
2018-01-22 13:22:17 +0800

    Update RankBattle1.ino

--- RankBattle1.ino
+++ RankBattle1.ino
@@ -16,9 +16,10 @@ static bool timetogo = false; //false for waiting for start, true for can remote
 WiFiClient wifiClient;
 static char buf[128], buf_send[128];
 static char client_ID[] = "NightKirie", Team[] = "A", BaseA = 'C', BaseB = 'C';
-static int step = 0, check = 0, hp = 0;
-static char *recv_ID, *recv_buf, *recv_mod;
-const int index = 2;
+static int hp = 0;
+
+//step for Dst1 to Dst2, check for things in step
+static char *recv_ID, *recv_buf, *recv_mod, name[32];
 
 enum MotorPinID
 {
@@ -32,16 +33,20 @@ enum MotorPinID
 class point
 {
     public:
-        point(int x = -1, int y = -1){
-            this->x = x;
-            this->y = y;
-        }
+        point(int x = -1, int y = -1);
         int x, y;
 };
 
+point::point(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
 //Dst1 for first destination(may not be my treasure)
-point DstPos, MyPos, lighthouse[3], MyDir, LastPos;
-const point TeamAPos(96, 96), TeamBPos(480, 480);
+point DstPos, MyPos, lighthouse[3];
+const point PosA(96, 96), PosB(480, 480);
+const int index = 2;
 
 // Pin assignment
 static const uint8_t motorPins[NUM_OF_MOTOR_PIN] = {14, 15, 16, 17};  //  L_F, L_B, R_F, R_B
@@ -69,15 +74,13 @@ void setup() {
         motorpins++;
     }
     Serial.begin(115200);
-    while (!Serial)
-        ;
+    while (!Serial);
     // set WiFi
     WiFi.begin(SSID, PASSWD);
     while (status != WL_CONNECTED) {
         // Connect failed, blink 0.5 second to indicate
         // the board is retrying.
         delay(500);
-        WiFi.begin(SSID, PASSWD);
         status = WiFi.begin(SSID, PASSWD);
         Serial.print("Attempting to connect to SSID: ");
         Serial.println(SSID);
@@ -100,23 +103,30 @@ void setup() {
 
 
 void askPos( void * parameter ) {
+    //get message from server
+    //format: Master|Words
+    //or like: Master|(1,2)(3,4)
+    //so we need to escape | and some characters
     while (1) {
         //read message from server
         if (wifiClient.available())
         {
-            for (int i = 0; i < 128 && buf[i-1] != '\r'; i++)
+            for (int i = 0; i < 128 && buf[i - 1] != '\r'; i++)
             {
                 buf[i] = wifiClient.read();
             }
             recv_ID = strtok(buf, "|");
-            recv_buf= strtok(NULL,"|");
+            Serial.println(recv_ID);
+            Serial.println("####################");
+            recv_buf = strtok(NULL, "|");
+            Serial.println(recv_buf);
             if (!strcmp(recv_ID, "Master")) {   //From Master
-                if (!strncmp(recv_buf, "Start",5))
+                if (!strncmp(recv_buf, "Start", 5))
                 { //Start
+                    Serial.println("Start!!");
                     timetogo = true;
-                    step = 0;
                 }
-                else if (!strcmp(recv_buf, "Done"))
+                else if (!strncmp(recv_buf, "Done", 4))
                 { //End
                     timetogo = false;
                 }
@@ -128,28 +138,16 @@ void askPos( void * parameter ) {
                         sscanf(recv_mod, "(%d, %d)BaseA:%cBaseB:%cTowers:(%d, %d)(%d, %d)(%d, %d)Blood:%d", &MyPos.x, &MyPos.y, &BaseA, &BaseB, &lighthouse[0].x, &lighthouse[0].y, &lighthouse[1].x, &lighthouse[1].y, &lighthouse[2].x, &lighthouse[2].y, &hp);
                     }
                     //          Serial.println("========================");
-                              Serial.println(lighthouse[0].x);
-                              Serial.println(lighthouse[0].y);
-                              Serial.println(lighthouse[1].x);
-                              Serial.println(lighthouse[1].y);
-                              Serial.println(lighthouse[2].x);
-                              Serial.println(lighthouse[2].y);
+                    //          Serial.println(lighthouse[index].x);
+                    //          Serial.println(lighthouse[index].y);
                     //          Serial.println(hp);
                     //          Serial.println("========================");
                     DstPos.x = lighthouse[index].x;
                     DstPos.y = lighthouse[index].y;
-
                 }
             }
-            if(timetogo == true){
+            if (timetogo) {
                 send_mes("Position", "");
-                if(BaseA == 'O')
-                    DstPos = TeamAPos;
-                else if(BaseB == 'O')
-                    DstPos = TeamBPos;
-                else{
-                    DstPos = lighthouse[0];
-                }
             }
         }
     }
@@ -191,52 +189,76 @@ void right(int t) {
     analogWrite(motorPins[R_B], 255);
     delay(t);
 }
-void slightly_left(int t) {
-    analogWrite(motorPins[L_F], 255);
+void slightly_left(int t, int angle) {
+    analogWrite(motorPins[L_F], 255 - angle);
     analogWrite(motorPins[L_B], 0);
     analogWrite(motorPins[R_F], 255);
     analogWrite(motorPins[R_B], 0);
     delay(t);
 }
-void slightly_right(int t) {
+void slightly_right(int t, int angle) {
     analogWrite(motorPins[L_F], 255);
     analogWrite(motorPins[L_B], 0);
-    analogWrite(motorPins[R_F], 150);
+    analogWrite(motorPins[R_F], 255 - angle);
     analogWrite(motorPins[R_B], 0);
     delay(t);
 }
 
 void loop()
 {
-    //it can't decide which destination to go
-    if(timetogo){
-        point LastPos(MyPos.x, MyPos.y);
-        forward(100);
-        freeze(0);
-        double DstDegree = atan2(DstPos.x - MyPos.x, DstPos.y - MyPos.y);
-        double MyDegree = atan2(MyPos.x - LastPos.x, MyPos.y - LastPos.y);
-        double Degree = MyDegree - DstDegree;
-        if(BaseA == 'O' && abs(MyPos.x - TeamAPos.x) < 30 && abs(MyPos.y - TeamAPos.y) < 30)
-            freeze(3000);
-        else if(BaseB == 'O' && abs(MyPos.x - TeamBPos.x) < 30 && abs(MyPos.y - TeamBPos.y) < 30)
-            freeze(3000);
-        else if(Degree < -0.05 || Degree > PI + 0.05){
-            if(Degree < -PI/2 || Degree > -PI*3/2){
-                right(300);
+    //for self-moving
+    double DstDir;
+    double MyDir;
+    double Degree;
+    if (timetogo) { //for go to lighthouse
+        point PrevPos(MyPos.x, MyPos.y);
+
+        //go to lighthouse
+        if (BaseA != 'O' && BaseB != 'O') {
+            forward(100);
+            DstDir = atan2(DstPos.y - PrevPos.y, DstPos.x - PrevPos.x);
+            MyDir = atan2(MyPos.y - PrevPos.y, MyPos.x - PrevPos.x);
+            Degree = MyDir - DstDir;
+        } 
+        else if (BaseA == 'O') {
+            if (abs(MyPos.x - PosA.x) <= 25 && abs(MyPos.y - PosA.y) <= 25) { //if get to the Dst1
+                freeze(3500);
+                return;
             }
-            else{
-                slightly_right(75);
+            forward(100);
+            DstDir = atan2(PosA.y - PrevPos.y, PosA.x - PrevPos.x);
+            MyDir = atan2(MyPos.y - PrevPos.y, MyPos.x - PrevPos.x);
+            Degree = MyDir - DstDir;
+        } 
+        else if (BaseB == 'O') {
+            if (abs(MyPos.x - PosB.x) <= 25 && abs(MyPos.y - PosB.y) <= 25) { //if get to the Dst1
+                freeze(3500);
+                return;
             }
+            forward(100);
+            DstDir = atan2(PosB.y - PrevPos.y, PosB.x - PrevPos.x);
+            MyDir = atan2(MyPos.y - PrevPos.y, MyPos.x - PrevPos.x);
+            Degree = MyDir - DstDir;
         }
-        else if(Degree > 0.05){
-            if(Degree > PI/2){
-                left(300);
+
+
+        if (Degree < -0.1 || Degree > PI + 0.1) {
+            if (Degree < -PI / 2 || Degree > -PI * 3 / 2){
+                right(150);
+                PrevPos = MyPos;
+                forward(100);
             }
-            else{
-                slightly_left(75);
-            }
+            else
+                slightly_right(75, 75);
+        } 
+        else if (Degree > 0.1) {
+            if (Degree > PI / 2){
+                left(150);
+                PrevPos = MyPos;
+                forward(100);
+            }    
+            else
+                slightly_left(75, 75);
         }
-        else
-            freeze(100);
     }
 }
