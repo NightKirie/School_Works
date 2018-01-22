e445875c09403869d04ed064518ba7f1bea30d7d
NightKirie <qwer55113322@gmail.com>
2018-01-22 13:36:11 +0800

    Update RankBattle1.ino

--- RankBattle1.ino
+++ RankBattle1.ino
@@ -2,263 +2,303 @@
 #include <LWiFi.h>
 #include <task.h>
 #include <math.h>
-#include <FreeRTOS.h>
 
-
-#define SSID "2scream"
+#define SSID "1scream2.4G"
 #define PASSWD "2017scream"
-#define TCP_IP "192.168.0.101"
+#define TCP_IP "192.168.0.38"
+//#define TCP_IP_PHONE "192.168.0.31"
 #define TCP_PORT 5000
 
 static bool timetogo = false; //false for waiting for start, true for can remote
-
-//wificlient connect between car and wifiserver
 WiFiClient wifiClient;
-static char buf[128], buf_send[128];
-static char client_ID[] = "NightKirie", Team[] = "A", BaseA = 'C', BaseB = 'C';
-static int hp = 0;
+WiFiClient wifiClientPh;
+static char buf[48],bufPh[48],buf_send[32],buf_phsend[32];
+static char client_ID[] = " NightKirie",Team[] = "DWLT";
+static int messageLen,phmessageLen;
+static int MyPosX, MyPosY, DstPosX, DstPosY, detection = 0;
+static char *recv_ID,*recv_buf;
 
-//step for Dst1 to Dst2, check for things in step
-static char *recv_ID, *recv_buf, *recv_mod, name[32];
 
-enum MotorPinID
-{
+enum MotorPinID {
     L_F = 0,
     L_B,
     R_F,
     R_B,
     NUM_OF_MOTOR_PIN
 };
-
-class point
-{
-    public:
-        point(int x = -1, int y = -1);
-        int x, y;
+enum UltrasonicPinID {
+    U_F = 0,
+    U_L,
+    U_R,
+    NUM_OF_ULTRASONIC_PIN
 };
 
-point::point(int x, int y)
-{
-    this->x = x;
-    this->y = y;
-}
-
-//Dst1 for first destination(may not be my treasure)
-point DstPos, MyPos, lighthouse[3];
-const point PosA(96, 96), PosB(480, 480);
-const int index = 2;
-
-// Pin assignment
+/* Pin assignment */
+static const uint8_t usTrigPins[NUM_OF_ULTRASONIC_PIN] = {2, 4, 6};  // F, L, R
+static const uint8_t usEchoPins[NUM_OF_ULTRASONIC_PIN] = {3, 5, 7};  // F, L, R
 static const uint8_t motorPins[NUM_OF_MOTOR_PIN] = {14, 15, 16, 17};  //  L_F, L_B, R_F, R_B
+int step = 0;
 
-void reg_ID() {
-    strcpy(buf, "Register");
-    strcat(buf, Team);
-    strcat(buf, "|");
-    strcat(buf, client_ID);
-    wifiClient.write(buf, strlen(buf));
-    wifiClient.flush();
-}
 
-void send_mes(char ID[], char mes[]) {
-    sprintf(buf, "%s|%s", ID, mes);
-    wifiClient.write(buf, strlen(buf));
-    wifiClient.flush();
-}
 
-void setup() {
+IPAddress ip;
+void setup(){
     int motorpins = 0;
-    int status = WL_IDLE_STATUS;
-    while (motorpins < NUM_OF_MOTOR_PIN) {
-        pinMode(motorPins[motorpins], OUTPUT);
+    while(motorpins < NUM_OF_MOTOR_PIN){
+        pinMode(motorPins[motorpins],OUTPUT);
         motorpins++;
     }
+    int status = WL_IDLE_STATUS;
     Serial.begin(115200);
-    while (!Serial);
+    while (!Serial)
+        ;       
     // set WiFi
     WiFi.begin(SSID, PASSWD);
     while (status != WL_CONNECTED) {
-        // Connect failed, blink 0.5 second to indicate
-        // the board is retrying.
+    // Connect failed, blink 0.5 second to indicate
+    // the board is retrying.
         delay(500);
-        status = WiFi.begin(SSID, PASSWD);
+        WiFi.begin(SSID, PASSWD);
+        status =  WiFi.begin(SSID, PASSWD);
         Serial.print("Attempting to connect to SSID: ");
         Serial.println(SSID);
+        Serial.println(status);
     }
-
+    
     // Conenct to AP successfully
-    while (!wifiClient.connect(TCP_IP, TCP_PORT)) {
+    while (!wifiClient.connect(TCP_IP, TCP_PORT)){
+    //delay(300);
         Serial.print("Attempting to connect to SERVER: ");
         Serial.println(TCP_IP);
     }
+    /*while (!wifiClientPh.connect(TCP_IP_PHONE, TCP_PORT)){
+        delay(300);
+        Serial.print("Attempting to connect to PHONE SERVER: ");
+        Serial.println(TCP_IP_PHONE);
+    }*/
     reg_ID();
+    //delay(1000);
     xTaskCreate(
-            askPos,          /* Task function. */
-            "askPos",        /* String with name of task. */
-            10000,           /* Stack size in words. */
-            NULL,            /* Parameter passed as input of the task */
-            1,               /* Priority of the task. */
-            NULL);           /* Task handle. */
+                    askPos,          /* Task function. */
+                    "askPos",        /* String with name of task. */
+                    10000,            /* Stack size in words. */
+                    NULL,             /* Parameter passed as input of the task */
+                    1,                /* Priority of the task. */
+                    NULL);            /* Task handle. */
+}  
+double ultrasonicGetDistance(uint8_t trig, uint8_t echo){
+    double duration;
+    pinMode(trig, OUTPUT);
+    pinMode(echo, INPUT);
+    digitalWrite(trig, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trig, LOW);
+    duration = pulseIn(echo, HIGH, 20000L);
+    return duration / 29 / 2;
+}
+void reg_ID()
+{
+    strcpy(buf,"Register|");
+    strcat(buf,client_ID);
+    wifiClient.write(buf, strlen(buf));
+    wifiClient.flush();
 }
 
+/*void send_phone(int x,int y)
+{
+    sprintf(buf_phsend,"(%d,%d)",x,y);
+    wifiClientPh.write(buf_phsend, strlen(buf_phsend));
+    wifiClientPh.flush();
+}*/
 
-void askPos( void * parameter ) {
-    //get message from server
-    //format: Master|Words
-    //or like: Master|(1,2)(3,4)
-    //so we need to escape | and some characters
-    while (1) {
-        //read message from server
-        if (wifiClient.available())
-        {
-            for (int i = 0; i < 128 && buf[i - 1] != '\r'; i++)
-            {
-                buf[i] = wifiClient.read();
+void send_mes(char ID[],char mes[])
+{
+    sprintf(buf,"%s|%s",ID,mes);
+    wifiClient.write(buf, strlen(buf));
+    wifiClient.flush();
+}
+
+void askPos( void * parameter )
+{
+    while(1){
+        if ((messageLen = wifiClient.available()) > 0) {
+            int i = 0;
+            do{
+                buf[i++] = wifiClient.read();
+            }while(i<32 && buf[i-1]!='\r'); 
+            buf[i-1] = '\0';
+            recv_ID = strtok(buf,"|\0");
+            //Serial.print(recv_ID);
+            //Serial.print(":");
+            recv_buf = strtok(NULL,"|\0");
+            //Serial.println(recv_buf);
+            if(strcmp(recv_buf, "Start") == 0){
+                timetogo = true;
+                detection = 0;
             }
-            recv_ID = strtok(buf, "|");
-            Serial.println(recv_ID);
-            Serial.println("####################");
-            recv_buf = strtok(NULL, "|");
-            Serial.println(recv_buf);
-            if (!strcmp(recv_ID, "Master")) {   //From Master
-                if (!strncmp(recv_buf, "Start", 5))
-                { //Start
-                    Serial.println("Start!!");
-                    timetogo = true;
-                }
-                else if (!strncmp(recv_buf, "Done", 4))
-                { //End
-                    timetogo = false;
-                }
-                else { //Something else
-                    recv_mod = strtok(recv_buf, ":");
-                    if (!strncmp(recv_mod, "POS", 3)) {
-                        recv_mod = strtok(NULL, "\n");
-                        //            Serial.println(recv_mod);
-                        sscanf(recv_mod, "(%d, %d)BaseA:%cBaseB:%cTowers:(%d, %d)(%d, %d)(%d, %d)Blood:%d", &MyPos.x, &MyPos.y, &BaseA, &BaseB, &lighthouse[0].x, &lighthouse[0].y, &lighthouse[1].x, &lighthouse[1].y, &lighthouse[2].x, &lighthouse[2].y, &hp);
-                    }
-                    //          Serial.println("========================");
-                    //          Serial.println(lighthouse[index].x);
-                    //          Serial.println(lighthouse[index].y);
-                    //          Serial.println(hp);
-                    //          Serial.println("========================");
-                    DstPos.x = lighthouse[index].x;
-                    DstPos.y = lighthouse[index].y;
-                }
+            else if(strcmp(recv_buf, "Done") == 0){
+                timetogo = false;
+                detection = 0;
             }
-            if (timetogo) {
-                send_mes("Position", "");
+            else
+                sscanf(recv_buf,"POS:(%d,%d)(%d,%d)",&MyPosX,&MyPosY,&DstPosX,&DstPosY);
+            //Serial.println(MyPosX);
+            //Serial.println(MyPosY);
+            //send_phone(MyPosX,MyPosY); 
+            send_mes("Position","");
+        }
+    }
+    
+    //Serial.println("Ending task 1");
+    vTaskDelete( NULL );
+ 
+}
+void freeze(int t){
+    analogWrite(motorPins[L_F], 0);
+    analogWrite(motorPins[L_B], 0);
+    analogWrite(motorPins[R_F], 0);
+    analogWrite(motorPins[R_B], 0);
+    delay(t);
+}
+void forward(int t){
+    analogWrite(motorPins[L_F], 150);
+    analogWrite(motorPins[L_B], 0);
+    analogWrite(motorPins[R_F], 150);
+    analogWrite(motorPins[R_B], 0);
+    delay(t);
+}
+void backward(int t){
+    analogWrite(motorPins[L_F], 0);
+    analogWrite(motorPins[L_B], 150);
+    analogWrite(motorPins[R_F], 0);
+    analogWrite(motorPins[R_B], 150);
+    delay(t);
+}
+void left(int t){
+    analogWrite(motorPins[L_F], 0);
+    analogWrite(motorPins[L_B], 150);
+    analogWrite(motorPins[R_F], 150);
+    analogWrite(motorPins[R_B], 0);
+    delay(t);
+}
+void right(int t){
+    analogWrite(motorPins[L_F], 150);
+    analogWrite(motorPins[L_B], 0);
+    analogWrite(motorPins[R_F], 0);
+    analogWrite(motorPins[R_B], 150);
+    delay(t);
+}
+void slightly_left(int t){
+    analogWrite(motorPins[L_F], 0);
+    analogWrite(motorPins[L_B], 0);
+    analogWrite(motorPins[R_F], 150);
+    analogWrite(motorPins[R_B], 0);
+    delay(t);
+}
+void slightly_right(int t){
+    analogWrite(motorPins[L_F], 150);
+    analogWrite(motorPins[L_B], 0);
+    analogWrite(motorPins[R_F], 0);
+    analogWrite(motorPins[R_B], 0);
+    delay(t);
+} 
+void ultrasonictest(int t, float df, float dl, float dr){
+    Serial.println(df);
+    Serial.println(dl);
+    Serial.println(dr);
+    delay(t);
+}
+
+//for remote
+void remoteCommand()
+{
+    if(timetogo == true){
+    // Stop moving
+        if (bufPh[1] == 'E') {
+            freeze(0);
+        }
+        else{
+            switch (bufPh[0]) {
+                case 'F':   // Forward
+                    forward(0);
+                    break;
+                case 'B':   // Backward
+                    backward(0);
+                    break;
+                case 'L':   // Turn left
+                    left(0);
+                    break;
+                case 'R':   // Turn right
+                    right(0);
+                    break;
+                case 'Z':   // Report ultrasonic distance and color
+                //    reportUltrasonic();
+                //reportColorSensor();
+                    break;
             }
         }
     }
-    vTaskDelete(NULL);
-}
-
-void freeze(int t) {
-    analogWrite(motorPins[L_F], 0);
-    analogWrite(motorPins[L_B], 0);
-    analogWrite(motorPins[R_F], 0);
-    analogWrite(motorPins[R_B], 0);
-    delay(t);
-}
-void forward(int t) {
-    analogWrite(motorPins[L_F], 255);
-    analogWrite(motorPins[L_B], 0);
-    analogWrite(motorPins[R_F], 255);
-    analogWrite(motorPins[R_B], 0);
-    delay(t);
-}
-void backward(int t) {
-    analogWrite(motorPins[L_F], 0);
-    analogWrite(motorPins[L_B], 255);
-    analogWrite(motorPins[R_F], 0);
-    analogWrite(motorPins[R_B], 255);
-    delay(t);
-}
-void left(int t) {
-    analogWrite(motorPins[L_F], 0);
-    analogWrite(motorPins[L_B], 255);
-    analogWrite(motorPins[R_F], 255);
-    analogWrite(motorPins[R_B], 0);
-    delay(t);
-}
-void right(int t) {
-    analogWrite(motorPins[L_F], 255);
-    analogWrite(motorPins[L_B], 0);
-    analogWrite(motorPins[R_F], 0);
-    analogWrite(motorPins[R_B], 255);
-    delay(t);
-}
-void slightly_left(int t, int angle) {
-    analogWrite(motorPins[L_F], 255 - angle);
-    analogWrite(motorPins[L_B], 0);
-    analogWrite(motorPins[R_F], 255);
-    analogWrite(motorPins[R_B], 0);
-    delay(t);
-}
-void slightly_right(int t, int angle) {
-    analogWrite(motorPins[L_F], 255);
-    analogWrite(motorPins[L_B], 0);
-    analogWrite(motorPins[R_F], 255 - angle);
-    analogWrite(motorPins[R_B], 0);
-    delay(t);
+    else if(timetogo == false){
+        freeze(0);
+    }
 }
 
 void loop()
-{
+{   
+    double df, dl, dr;
+    df = ultrasonicGetDistance(usTrigPins[U_F], usEchoPins[U_F]);
+    dl = ultrasonicGetDistance(usTrigPins[U_L], usEchoPins[U_L]);
+    dr = ultrasonicGetDistance(usTrigPins[U_R], usEchoPins[U_R]);
+    //for remote
+    /*if ((phmessageLen = wifiClientPh.available()) > 0) {
+        bufPh[0] = wifiClientPh.read();
+        bufPh[1] = wifiClientPh.read();
+        remoteCommand();
+    }*/
     //for self-moving
-    double DstDir;
-    double MyDir;
-    double Degree;
-    if (timetogo) { //for go to lighthouse
-        point PrevPos(MyPos.x, MyPos.y);
-
-        //go to lighthouse
-        if (BaseA != 'O' && BaseB != 'O') {
-            forward(100);
-            DstDir = atan2(DstPos.y - PrevPos.y, DstPos.x - PrevPos.x);
-            MyDir = atan2(MyPos.y - PrevPos.y, MyPos.x - PrevPos.x);
-            Degree = MyDir - DstDir;
-        } 
-        else if (BaseA == 'O') {
-            if (abs(MyPos.x - PosA.x) <= 25 && abs(MyPos.y - PosA.y) <= 25) { //if get to the Dst1
-                freeze(0);
-                return;
+    if(timetogo == true){   //for game start
+        static const int InitPosX = MyPosX;
+        static const int InitPosY = MyPosY;
+        if(detection == 0){
+            forward(0);
+            if(abs(InitPosX - MyPosX) > 200 || abs(InitPosY - MyPosY) > 200){
+                freeze(100);
+                static const int MidDirX = MyPosX - InitPosX;
+                static const int MidDirY = MyPosY - InitPosY;
+                static const double MidDir = atan2(MidDirY, MidDirX);
+                static const int InitDirX = DstPosX - InitPosX;
+                static const int InitDirY = DstPosY - InitPosY;
+                static const double InitDir = atan2(InitDirY, InitDirX);
+                if(MidDir - InitDir < 0 || MidDir - InitDir > PI)
+                    right(250);
+                else if(MidDir - InitDir > 0)
+                    left(250);    
+                detection = 1;
             }
-            forward(100);
-            DstDir = atan2(PosA.y - PrevPos.y, PosA.x - PrevPos.x);
-            MyDir = atan2(MyPos.y - PrevPos.y, MyPos.x - PrevPos.x);
-            Degree = MyDir - DstDir;
-        } 
-        else if (BaseB == 'O') {
-            if (abs(MyPos.x - PosB.x) <= 25 && abs(MyPos.y - PosB.y) <= 25) { //if get to the Dst1
-                freeze(0);
-                return;
-            }
-            forward(100);
-            DstDir = atan2(PosB.y - PrevPos.y, PosB.x - PrevPos.x);
-            MyDir = atan2(MyPos.y - PrevPos.y, MyPos.x - PrevPos.x);
-            Degree = MyDir - DstDir;
         }
-
-
-        if (Degree < -0.1 || Degree > PI + 0.1) {
-            if (Degree < -PI / 2 || Degree > -PI * 3 / 2){
-                right(150);
-                PrevPos = MyPos;
-                forward(150);
+        if(detection == 1){
+            static int MidPosX = MyPosX;
+            static int MidPosY = MyPosY; 
+            static const int EndDirX = DstPosX - MidPosX;
+            static const int EndDirY = DstPosY - MidPosY;
+            static const double EndDir = atan2(EndDirY, EndDirX);
+            forward(0);
+            delay(50);
+            double MyDir = atan2(MyPosY - MidPosY, MyPosX - MidPosX);
+            if(MyDir - EndDir < 0 || MyDir - EndDir > PI){
+                slightly_right(50);
+                forward(50);
             }
-            else
-                slightly_right(75, 75);
-        } 
-        else if (Degree > 0.1) {
-            if (Degree > PI / 2){
-                left(150);
-                PrevPos = MyPos;
-                forward(150);
-            }    
-            else
-                slightly_left(75, 75);
+            else if(MyDir - EndDir > 0){
+                slightly_left(50);
+                forward(50);
+            }
+            else{
+                forward(0);    
+            }
         }
     }
-}
+    else if(timetogo == false){     //for game end
+        freeze(0);
+    }
+} 
